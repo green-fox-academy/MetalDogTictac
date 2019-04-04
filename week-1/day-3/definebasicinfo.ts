37195949da369ef8b71d835e96bd9ccc0bf9b84b
MetalDogL <lilkavanilka@gmail.com>
2019-04-04 16:02:51 +0200

    add definebasicinfo.ts

---
+++ week-1/day-3/definebasicinfo.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = 'Lili';
+let age: number = 34;
+let height: number = 1.8;
+let amImarried: boolean = false;
+
+console.log(myName);
+console.log(age);
+console.log(height);
+console.log(amImarried);
