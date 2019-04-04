9a9497db9d5554bbe8017cf46097458264699897
MetalDogL <lilkavanilka@gmail.com>
2019-04-04 15:56:40 +0200

    add bmi.ts

---
+++ week-1/day-3/bmi.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let massInKg: number = 75;
+let heightInM: number = 1.80;
+
+// Print the Body mass index (BMI) based on these values
+
+console.log(massInKg / (heightInM * heightInM));
+
