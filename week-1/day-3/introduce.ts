6a8d9ce9684f255495ceb147a78e511592a22770
MetalDogL <lilkavanilka@gmail.com>
2019-04-04 13:53:50 +0200

    add introduce.ts

---
+++ week-1/day-3/introduce.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+let myName: string = 'Lili';
+let age: number = 34;
+let height: number = 1.8;
+
+console.log(myName);
+console.log(age);
+console.log(height);
