8c7e26f4cc1efc8c4420ed4641d8a0a37ae2873f
MetalDogL <lilkavanilka@gmail.com>
2019-04-04 15:06:19 +0200

    add twonumbers.ts

---
+++ week-1/day-3/twonumbers.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the reminder of 22 divided by 13
+
+let number1: number = 22;
+let number2: number = 13;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+
