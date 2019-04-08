9b3e35de5e48ce2ff200ac63b80e2ed957123b32
MetalDogL <lilkavanilka@gmail.com>
2019-04-08 16:48:08 +0200

    feat(w2,d1) add appendA.ts

---
+++ week-2/day-1-functions-arrays/appendA.ts
@@ -0,0 +1,13 @@
+"use strict";
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string[] = ["koal", "pand", "zebr"];
+
+let animalsWithCorrectNames = animals.map(function(e) {
+  return e + "a";
+});
+
+console.log(animalsWithCorrectNames);
