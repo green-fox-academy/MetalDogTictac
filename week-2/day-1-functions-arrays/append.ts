6c03e8febf08973eb50124993742c9a9d4a1283c
MetalDogL <lilkavanilka@gmail.com>
2019-04-08 14:25:52 +0200

    feat(w2,d1) add append.ts

---
+++ week-2/day-1-functions-arrays/append.ts
@@ -0,0 +1,14 @@
+"use strict";
+export {};
+// -  Create a string variable named `typo` and assign the value `Chinchill` to it
+// -  Write a function called `appendAFunc` that gets a string as an input,
+//    appends an 'a' character to its end and returns with a string
+// -  Print the result of `appendAFunc(typo)`
+
+let typo: string = "Chinchill";
+
+function appendAFun(str): string {
+  return str + "a";
+}
+
+console.log(appendAFun(typo));
