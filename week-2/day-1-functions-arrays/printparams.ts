49bab1dbc9daf3588d8a837772ee32d6e35b5e4f
MetalDogL <lilkavanilka@gmail.com>
2019-04-08 15:31:06 +0200

    feat(w2,d1) add printparams.ts

---
+++ week-2/day-1-functions-arrays/printparams.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams (par1, par2): void {
+  console.log(par1, par2);
+}
+
+console.log(printParams(2, 5));
