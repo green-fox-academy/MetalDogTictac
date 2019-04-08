07ae1c741e93414d0aa6a70a9dfef51302cd74f6
MetalDogL <lilkavanilka@gmail.com>
2019-04-08 17:23:06 +0200

    feat(w2,d1) add option with loop

--- week-2/day-1-functions-arrays/reverselist.ts
+++ week-2/day-1-functions-arrays/reverselist.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // -  Create a variable named `numList`
 //    with the following content: `[3, 4, 5, 6, 7]`
@@ -9,6 +9,20 @@
 
 let numList: number[] = [3, 4, 5, 6, 7];
 
+//done with a built-in method
+
 let newList: number[] = numList.reverse();
 
 console.log(newList);
+
+//done with a loop
+
+let reversedList: number[] = [];
+
+for (let i = numList.length - 1; i >= 0; i--) {
+  reversedList.push(numList[i]);
+}
+
+numList = reversedList;
+
+console.log(numList);
