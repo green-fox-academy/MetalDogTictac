4163c82c00e2f1b7ad3987bbc343bbb9cf8f2f1a
MetalDogL <lilkavanilka@gmail.com>
2019-04-11 11:33:35 +0200

    feat(w2,d4) add bunnies2.ts

---
+++ week-2/day-4-recursion/bunnies2.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+function bunnyEarCounter(numberOfBunnies: number) {
+  if (numberOfBunnies <= 0) {
+    return 0;
+  } else if (numberOfBunnies === 1) {
+    return 2;
+  } else if (numberOfBunnies % 2 === 0) {
+    return 3 + bunnyEarCounter(numberOfBunnies - 1);
+  } else if (numberOfBunnies % 2 !== 0) {
+    return 2 + bunnyEarCounter(numberOfBunnies - 1);
+  }
+}
+
+console.log(bunnyEarCounter(3));
