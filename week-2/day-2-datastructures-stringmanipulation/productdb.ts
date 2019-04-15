e22faca24358d8ee9a61214cd8f336ca306aad24
MetalDogL <lilkavanilka@gmail.com>
2019-04-15 15:08:04 +0200

    feat(w2,d2) add first part of productdb.ts

---
+++ week-2/day-2-datastructures-stringmanipulation/productdb.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+var foodItems: any = {
+  'eggs': 200,
+  'milk': 200,
+  'fish': 400,
+  'apples': 150,
+  'bread': 50,
+  'chicken': 550
+}
+
+//saving data in vars
+let foodz = Object.keys(foodItems);
+let pricez = Object.values(foodItems);
+
+//how much is the fish
+console.log(foodItems['fish']);
+
+//find out the most expensive product
+
+
+
