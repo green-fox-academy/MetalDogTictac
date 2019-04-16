7c50772d8159d7d08b85a21411fc82365d52218a
MetalDogL <lilkavanilka@gmail.com>
2019-04-16 11:53:16 +0200

    feat(w3,d2) update if function

--- week-3/day-2/dividebyzero.ts
+++ week-3/day-2/dividebyzero.ts
@@ -5,10 +5,10 @@
 // and prints the result.
 // It should print 'fail' if the parameter is 0
 
-function divideByZero (num) {
-  10 / num;
-
-  if (num === 0) {
+function divideByZero (num: number = 10) {
+  if (num !== 0) {
+    console.log(10 / num);
+  } else {
     console.log('fail');
   }
 }
