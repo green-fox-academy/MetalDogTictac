19dc3399c66ebd6e9ac925910fb4442bd72542ef
MetalDogL <lilkavanilka@gmail.com>
2019-04-16 12:05:08 +0200

    feat(w3,d2) add countlines.ts

---
+++ week-3/day-2/countlines.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+const fs = require("fs");
+
+function countTheLines(fileName: string) {
+  try {
+    let fileContent = fs.readFileSync("my-file.txt", "UTF-8");
+    let numberOfSentences = fileContent.split("\n");
+
+    if (fileContent) {
+      console.log(numberOfSentences.length);
+    } else {
+      console.log("0");
+    }
+  } catch (e) {}
+}
+
+countTheLines("my-file.txt");
