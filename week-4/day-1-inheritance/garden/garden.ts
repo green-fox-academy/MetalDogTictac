01120ad0c4238500eec156be548555b6e824d533
MetalDogL <lilkavanilka@gmail.com>
2019-04-23 14:56:48 +0200

    feat(w4,d1) garden checkwaterlevel works now

--- week-4/day-1-inheritance/garden/garden.ts
+++ week-4/day-1-inheritance/garden/garden.ts
@@ -4,9 +4,9 @@ import { Flower } from "./flower";
 import { Tree } from "./tree";
 import { Plant } from "./plant";
 
-export class Garden extends Plant {
-  flowers: Flower[];
-  trees: Tree[];
+class Garden {
+  flowers: Flower[] = [];
+  trees: Tree[] = [];
 
   addFlower(newFlower: Flower) {
     this.flowers.push(newFlower);
@@ -19,16 +19,55 @@ export class Garden extends Plant {
   startWatering(waterAmount: number) {
     let plantsThatNeedToBeWatered = [];
 
-    for (let f: number = 0; f <= this.flowers.length; f++) {
+    for (let f: number = 0; f < this.flowers.length; f++) {
       if (this.flowers[f].waterLevel < 5) {
         plantsThatNeedToBeWatered.push(this.flowers[f]);
       }
     }
 
     for (let t: number = 0; t < this.trees.length; t++) {
-      if (this.trees[t].waterLevel < 10, t++) {
+      if (this.trees[t].waterLevel < 10) {
         plantsThatNeedToBeWatered.push(this.trees[t]);
       }
     }
+
+    for (let k = 0; k < plantsThatNeedToBeWatered.length; k++) {
+      plantsThatNeedToBeWatered[k].watering(
+        waterAmount / plantsThatNeedToBeWatered.length
+      );
+    }
   }
-}
+
+  checkWaterLevel(): void {
+    for (let i: number = 0; i < this.flowers.length; i++) {
+      if (this.flowers[i].waterLevel < 5) {
+        console.log('The ' + this.flowers[i].color + ' Flower needs water.');
+      } else {
+        console.log('The ' + this.flowers[i].color + ' Flower doesn\'t need water.');
+      }
+    }
+
+    for (let j: number = 0; j < this.trees.length; j++) {
+      if (this.trees[j].waterLevel < 10) {
+        console.log('The ' + this.trees[j].color + ' Tree needs water.');
+      } else {
+        console.log('The ' + this.trees[j].color + ' Tree doesn\'t need water.');
+      }
+      }
+    }
+  }
+
+const flower1 = new Flower("yellow", 4);
+const flower2 = new Flower("blue", 4);
+const tree1 = new Tree("purple", 8);
+const tree2 = new Tree("orange", 8);
+
+let garden1: Garden = new Garden();
+garden1.addFlower(flower1);
+garden1.addFlower(flower2);
+garden1.addTree(tree1);
+garden1.addTree(tree2);
+
+garden1.checkWaterLevel();
+
+
