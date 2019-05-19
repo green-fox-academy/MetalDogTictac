5fa5f613a3b642d526f57254767e25b22526831f
Lili Ivany <lilkavanilka@gmail.com>
2019-05-19 20:40:43 +0200

    add finished files for Greetings task

---
+++ week-6/day-4-server-basics-templating/Greetings/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// home page
+app.get("/", (req, res) => {
+  // render `home.ejs`
+  let guestName = 'req.query.name';
+  if (!guestName) {
+    guestName = "Guest";
+  }
+  res.render("home", {
+    title: `Welcome back, ${guestName}!`
+  });
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
