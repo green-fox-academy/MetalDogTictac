d01b68c9251c460b0488a2cc2b013bf08a3d0b77
Lili Ivany <lilkavanilka@gmail.com>
2019-05-21 12:09:11 +0200

    add simpletimeout1.js

---
+++ week-7/day-2-callback-xhr/callbackTasks/simpletimeout1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+setTimeout(() => {
+    console.log('apple after 3sec'); // prints after one second
+  }, 3000);
+  console.log('first apple'); // prints first
