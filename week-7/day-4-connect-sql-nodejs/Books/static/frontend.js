2841c26a54f8a0b20899ddcd11d9d65660695a33
Lili Ivany <lilkavanilka@gmail.com>
2019-05-23 22:29:01 +0200

    feat(frontend) add first version of the new table

--- week-7/day-4-connect-sql-nodejs/Books/static/frontend.js
+++ week-7/day-4-connect-sql-nodejs/Books/static/frontend.js
@@ -10,15 +10,30 @@ httpRequest.open("GET", "http://localhost:3000/getbooktitles");
 
 //console.log(data.target.response);
 
-httpRequest.onload = data => {
-  //console.log(data);
+httpRequest.onload = () => {
+  let allData = JSON.parse(httpRequest.responseText);
+  let newTableBody = document.querySelector("tablebody");
 
-  let bookTitles = JSON.parse(data.target.response);
-
-  for (let i = 0; i < bookTitles.length; i++) {
-    let uls = document.getElementsByTagName("ul")[0];
-    uls.appendChild(document.createElement("li")).innerText =
-      bookTitles[i].book_name;
+  for (let i = 0; i < allData.length; i++) {
+    let newTableRow = document.createElement("tr");
+    let newBookTitle = document.createElement("td");
+    let newAuthor = document.createElement("td");
+    let newCategory = document.createElement("td");
+    let newPublisher = document.createElement("td");
+    let newBookPrice = document.createElement("td");
+    newBookTitle.innerHTML = allData[i].book_name;
+    newAuthor.innerHTML = allData[i].aut_name;
+    newCategory.innerHTML = allData[i].cate_descrip;
+    newCategory.className = "category";
+    newPublisher.innerHTML = allData[i].pub_name;
+    newPublisher.className = "publisher";
+    newBookPrice.innerHTML = allData[i].book_price;
+    newBookPrice.className = " price";
+    newTableBody.appendChild(newTableRow).appendChild(newBookTitle);
+    newTableBody.appendChild(newTableRow).appendChild(newAuthor);
+    newTableBody.appendChild(newTableRow).appendChild(newCategory);
+    newTableBody.appendChild(newTableRow).appendChild(newPublisher);
+    newTableBody.appendChild(newTableRow).appendChild(newBookPrice);
   }
 };
 
