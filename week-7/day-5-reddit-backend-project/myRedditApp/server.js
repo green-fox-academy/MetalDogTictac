2ae528ac938bd687658c5a0acba39f5b94381d11
Lili Ivany <lilkavanilka@gmail.com>
2019-05-29 11:37:01 +0200

    chore(redditbackend) remove node_modules folder that was unnecessary to be pushed

--- week-7/day-5-reddit-backend-project/myRedditApp/server.js
+++ week-7/day-5-reddit-backend-project/myRedditApp/server.js
@@ -5,14 +5,16 @@ const mysql = require("mysql");
 const app = express();
 const PORT = 3000;
 const path = require("path");
+const POST_TABLE = "posts";
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/static")));
 
 const mySQLConn = mysql.createConnection({
   host: "localhost",
   user: "lili",
-  password: "12345678"
-  //database: ""
+  password: "12345678",
+  database: "reddit"
 });
 //if this error: Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client
 //run: ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
@@ -25,11 +27,14 @@ mySQLConn.connect(err => {
   console.log("connection to DB is OK ✨");
 });
 
-app.use(express.static(path.join(__dirname, "/static")));
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+  })
 
 app.listen(PORT, () => {
   console.log("Server started on port 3000");
